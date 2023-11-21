82f04e3a7af74744160cfe92e76c64e0eb1e47ef
Artur <61761596+Ixperio@users.noreply.github.com>
2023-11-21 23:59:38 +0100

    Add files via upload

---
+++ Grafik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Grafik
+    {
+        public DateTime Data { get; set; }
+        public int Id_Funkcjonariusza {  get; set; }
+        public string Godzina_Przyjscia { get; set; }
+        public string Godzina_Wyjscia { get; set; }
+        public int Id_Zmiany {  get; set; }
+        public virtual Zmiana Zmiana { get; set; }
+        public virtual Funkcjonariusz Funkcjonariusz { get; set; }  
+    }
+}
